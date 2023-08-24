2ba87c135e33d02a1c067e876c658ea26d11f85a
Om Mani Tripathi <76935835+OmMani002@users.noreply.github.com>
2023-08-24 22:20:59 +0530

    Update db.js

--- hospital-management-system-main/config/db.js
+++ hospital-management-system-main/config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://maniom002:R4IMQ27aynjn11Qr@mern-project.j4somtb.mongodb.net/DOCTOR-APPOINTMENT");
+    await mongoose.connect("mongodb+srv://maniom002:<password>@mern-project.j4somtb.mongodb.net/DOCTOR-APPOINTMENT");
     // console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     // console.log(`Mongodb Server Issue ${error}`.bgRed.white);
